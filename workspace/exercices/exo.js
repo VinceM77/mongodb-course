0116f026b89255708b17e627bde47bc0973ec81d
Vince_M <vinzmdark@gmail.com>
2023-08-11 10:10:53 +0200

    correction exo en groupe

--- workspace/exercices/exo.js
+++ workspace/exercices/exo.js
@@ -11,6 +11,44 @@ const movieWithKeanu = db.movies.find({
 
 console.log(movieWithKeanu)
 
+// corrections
+
+const keanuReavesFilms = db.movies.find({
+    cast: 'Keanu Reeves'
+}).projection({
+    title: 1
+});
+
+console.log(keanuReavesFilms);
+
+const comedies = db.movies.find({
+    genres: 'Comedy'
+}).projection({
+    genres: 1,
+    title: 1
+})
+
+console.log(comedies);
+
+const between2002And2008 = db.movies.find({
+    year: {
+        $gte: 2002,
+        $lte: 2008 
+    }
+}).sort({
+    year: 1
+});
+
+console.log(between2002And2008);
+
+const mattDiamant = db.movies.find({
+    cast: {
+        $all: ['Chris O\'Donnell','Matt Damon']
+    }
+});
+
+console.log(mattDiamant);
+
 
 // récuperer les clés de la bdd sample_mflix
 var keys = [];
