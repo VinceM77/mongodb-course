db0e6afe9fdaa5a661307f9f0613f4087c494115
Vince_M <vinzmdark@gmail.com>
2023-08-11 09:55:39 +0200

    correction exo

--- workspace/exercices/exo3.js
+++ workspace/exercices/exo3.js
@@ -1,12 +1,15 @@
 let db = connect("mongodb://root:test123*@localhost:27017")
 .getSiblingDB("sample_mflix");
 
-const comedie = db.movies.find({
-    genres : 'Comedy',
+const movieBetween2002And2008 = db.movies.find({
+    year : {
+    $gte: 2002,
+    $lte:2008,
+    },
     
 }).projection({
     genres : 1,
     title:1,
 });
 
-console.log(comedie)
+console.log()
